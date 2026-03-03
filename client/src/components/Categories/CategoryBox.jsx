582c4fb19cca18594fac30a188266eb436762b1c
SaPa36 <nursapa2001@gmail.com>
2026-03-03 15:16:07 +0600

    add a actuve border which category selected

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
 import queryString from 'query-string'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params,setParams] = useSearchParams()
+  const category = params.get('category')
 
   const navigate = useNavigate()
 
@@ -26,7 +28,8 @@ const CategoryBox = ({ label, icon: Icon }) => {
   border-b-2
   hover:text-neutral-800
   transition
-  cursor-pointer`}
+  cursor-pointer 
+  ${category === label ? 'border-b-neutral-800' : 'border-transparent'}`}
     >
       <Icon size={26} />
       <div className='text-sm font-medium'>{label}</div>
