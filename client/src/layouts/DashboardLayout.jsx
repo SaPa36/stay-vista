ca8d0846c4c9097eb717493948af286128c55bac
SaPa36 <nursapa2001@gmail.com>
2026-03-04 11:18:45 +0600

    added sidebar

---
+++ client/src/layouts/DashboardLayout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/Dashboard/Sidebar/Sidebar';
+
+const DashboardLayout = () => {
+    return (
+        <div>
+            <div className='relative min-h-screen md:flex'>
+                {/* Sidebar */}
+                <Sidebar />
+
+                {/* Outlet --> Dynamic content */}
+                <div className='flex-1 md:ml-64'>
+                    <div className='p-5'>
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
