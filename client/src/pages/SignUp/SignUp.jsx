2dfc2b3df0efa509e3fb63d37eaccdf3c70c5059
SaPa36 <nursapa2001@gmail.com>
2026-03-04 09:58:58 +0600

    fixed signup

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,15 +1,21 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
+import useAuth from '../../hooks/useAuth'
 import axios from 'axios'
-import { useContext } from 'react'
-import { AuthContext } from '../../providers/AuthProvider'
 import toast from 'react-hot-toast'
+import { TbFidgetSpinner } from 'react-icons/tb'
 
 const SignUp = () => {
-  const {createUser, signInWithGoogle, updateUserProfile, loading, setLoading} = useContext(AuthContext)
   const navigate = useNavigate()
+  const {
+    createUser,
+    signInWithGoogle,
+    updateUserProfile,
+    loading,
+    setLoading,
+  } = useAuth()
 
-  const handleSignUp = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault()
     const form = e.target
     const name = form.name.value
@@ -17,30 +23,46 @@ const SignUp = () => {
     const password = form.password.value
     const image = form.image.files[0]
     const formData = new FormData()
-    formData.append('name', name)
-    formData.append('email', email)
-    formData.append('password', password)
     formData.append('image', image)
 
     try {
       setLoading(true)
-      const { data } = await axios.post(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
-         formData)
-      const user = await createUser(email, password)
-      
+      // 1. Upload image and get image url
+      const { data } = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${
+          import.meta.env.VITE_IMGBB_API_KEY
+        }`,
+        formData
+      )
+      console.log(data.data.display_url)
 
-      await updateUserProfile(name, data.data.url)
-      console.log(data)
-      navigate('/login')
-      toast.success('Signup successful! Please login to continue.')
-      setLoading(false)
+      //2. User Registration
+      const result = await createUser(email, password)
+      console.log(result)
 
+      // 3. Save username and photo in firebase
+      await updateUserProfile(name, data.data.display_url)
+      navigate('/')
+      toast.success('Signup Successful')
     } catch (err) {
       console.log(err)
-      toast.error('Signup failed. Please try again.')
-      setLoading(false)
+      toast.error(err.message)
     }
   }
+
+  // handle google signin
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle()
+
+      navigate('/')
+      toast.success('Signup Successful')
+    } catch (err) {
+      console.log(err)
+      toast.error(err.message)
+    }
+  }
+
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -48,10 +70,7 @@ const SignUp = () => {
           <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
           <p className='text-sm text-gray-400'>Welcome to StayVista</p>
         </div>
-        <form
-          onSubmit={handleSignUp}
-          className='space-y-6 '
-        >
+        <form onSubmit={handleSubmit} className='space-y-6'>
           <div className='space-y-4'>
             <div>
               <label htmlFor='email' className='block mb-2 text-sm'>
@@ -112,12 +131,15 @@ const SignUp = () => {
 
           <div>
             <button
-            disabled={loading}
+              disabled={loading}
               type='submit'
               className='bg-rose-500 w-full rounded-md py-3 text-white'
             >
-              {loading ? 'Signing Up...' : 'Sign Up'}
-              Continue
+              {loading ? (
+                <TbFidgetSpinner className='animate-spin m-auto' />
+              ) : (
+                'Continue'
+              )}
             </button>
           </div>
         </form>
@@ -128,11 +150,15 @@ const SignUp = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button
+          disabled={loading}
+          onClick={handleGoogleSignIn}
+          className='disabled:cursor-not-allowed flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
+        >
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
+        </button>
         <p className='px-6 text-sm text-center text-gray-400'>
           Already have an account?{' '}
           <Link
